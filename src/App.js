9f49bda49799bdbd23a0da8beaf7cabc9e7d6231
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-11-08 13:44:19 -0500

    add

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Navigation from  './Component/Navigation'
+import Footer from './Component/Footer'
+
+const App = () => {
+
+  return (
+   <>
+  <Navigation/>
+  <Footer/>
+   </>
+  )
+}
+
+export default App
