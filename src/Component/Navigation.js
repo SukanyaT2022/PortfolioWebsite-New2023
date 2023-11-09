a71172452002e4fc27898f79d701cb4e9d86e9e6
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-11-09 09:30:00 -0500

    add

--- src/Component/Navigation.js
+++ src/Component/Navigation.js
@@ -1,173 +1,24 @@
-// import { Fragment } from 'react'
-// import { Disclosure, Menu, Transition } from '@headlessui/react'
-// import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-// import logo from './image/logo'
 
-// const Navigation = () => {
-//   return (
-//     <div>
-//       <nav class="bg-[#ff6787]">
-//         <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-//           <div class="relative flex h-16 items-center justify-between">
-//             <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-//               <button
-//                 type="button"
-//                 class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-//                 aria-controls="mobile-menu"
-//                 aria-expanded="false"
-//               >
-//                 <span class="absolute -inset-0.5"></span>
-//                 <span class="sr-only">Open main menu</span>
-
-//                 <svg
-//                   class="block h-6 w-6"
-//                   fill="none"
-//                   viewBox="0 0 24 24"
-//                   stroke-width="1.5"
-//                   stroke="currentColor"
-//                   aria-hidden="true"
-//                 >
-//                   <path
-//                     stroke-linecap="round"
-//                     stroke-linejoin="round"
-//                     d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-//                   />
-//                 </svg>
-
-//                 <svg
-//                   class="hidden h-6 w-6"
-//                   fill="none"
-//                   viewBox="0 0 24 24"
-//                   stroke-width="1.5"
-//                   stroke="currentColor"
-//                   aria-hidden="true"
-//                 >
-//                   <path
-//                     stroke-linecap="round"
-//                     stroke-linejoin="round"
-//                     d="M6 18L18 6M6 6l12 12"
-//                   />
-//                 </svg>
-//               </button>
-//             </div>
-//             <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-//               <div class="flex flex-shrink-0 items-center">
-//                 <img class="h-8 w-auto" src="../images/logo.png" alt="Logo" />
-//               </div>
-//               <div class="hidden sm:ml-6 sm:block">
-//                 <div class="flex space-x-4">
-//                   <a
-//                     href="#"
-//                     class="bg-gray-900 text-black rounded-md px-3 py-2 text-sm font-medium"
-//                     aria-current="page"
-//                   >
-//                     Home
-//                   </a>
-//                   <a
-//                     href="#"
-//                     class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-//                   >
-//                     Resume
-//                   </a>
-//                   <a
-//                     href="#"
-//                     class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-//                   >
-//                     About Me
-//                   </a>
-//                   <a
-//                     href="#"
-//                     class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-//                   >
-//                     Portfolio
-//                   </a>
-//                   <a
-//                     href="#"
-//                     class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-//                   >
-//                     React Projects
-//                   </a>
-//                   <a
-//                     href="#"
-//                     class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-//                   >
-//                     IOS & Swift Projects
-//                   </a>
-//                   <a
-//                     href="#"
-//                     class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-//                   >
-//                     Contact
-//                   </a>
-//                 </div>
-//               </div>
-//             </div>
-//             <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-//               <div class="relative ml-3">
-//                 <div>
-//                   <a className="border-2 border-white rounded-xl text-white py-2 px-4" href="CVupdateAug2023.pdf" download="">
-//                     <button class="button-downloadCV">
-//                       Download CV
-//                       <i className="fa-solid fa-chevron-down"></i>
-//                     </button>
-//                   </a>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-
-//         <div class="sm:hidden" id="mobile-menu">
-//           <div class="space-y-1 px-2 pb-3 pt-2">
-//             <a
-//               href="#"
-//               class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
-//               aria-current="page"
-//             >
-//               Dashboard
-//             </a>
-//             <a
-//               href="#"
-//               class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-//             >
-//               Team
-//             </a>
-//             <a
-//               href="#"
-//               class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-//             >
-//               Projects
-//             </a>
-//             <a
-//               href="#"
-//               class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-//             >
-//               Calendar
-//             </a>
-//           </div>
-//         </div>
-//       </nav>
-//     </div>
-//   );
-// };
-// export default Navigation;
-
-// import { Fragment } from 'react'
-// import { Disclosure, Menu, Transition } from '@headlessui/react'
-// import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import myLogo from '../Images/logo.png'
 
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  { name: 'Home', href: '#', current: true },
+  { name: 'Resume', href: '#', current: false },
+  { name: 'About Me', href: '#', current: false },
+  { name: 'Portfolio', href: '#', current: false },
+  { name: 'React Projects', href: '#', current: false },
+  { name: 'IOS & Swift Projects', href: '#', current: false },
+  { name: 'Contact', href: '#', current: false },
 ]
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Example() {
+ function Navigation() {
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
@@ -190,8 +41,8 @@ export default function Example() {
                 <div className="flex flex-shrink-0 items-center">
                   <img
                     className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
+                    src={myLogo}
+                    alt="logo"
                   />
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
---
+++ src/Data.js
@@ -0,0 +1,29 @@
+dataPortfolio = [
+{
+    id : "1p",
+    title: 'Guess Word Game',
+    detail: "Built with: JavaScript, DynamicHTML, and CSS",
+    myLink: ""
+}
+
+],
+
+dataReact = [
+    {
+        id : "1r",
+        title: 'Guess Word Game',
+        detail: "Built with: JavaScript, DynamicHTML, and CSS",
+        myLink: ""
+    }
+    
+    ],
+    dataSwift = [
+        {
+            id : "1s",
+            title: 'Guess Word Game',
+            detail: "Built with: JavaScript, DynamicHTML, and CSS",
+            myLink: ""
+        }
+        
+        ]
+        export default {dataPortfolio,dataReact, dataSwift}
