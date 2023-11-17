c921e0a50b13f1fdc073daaf3c0091e3a874b9e8
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-11-17 06:44:37 -0500

    add

--- src/Component/Navigation.js
+++ src/Component/Navigation.js
@@ -22,10 +22,10 @@ function classNames(...classes) {
  function Navigation() {
   return (
     // control whole nav bar
-    <Disclosure as="nav" className="bg-[#ff6787]">
+    <Disclosure as="nav" className="bg-[#eeeeee]">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 md:py-2 ">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
@@ -40,21 +40,25 @@ function classNames(...classes) {
                 </Disclosure.Button>
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                {/* targetLogo */}
                 <div className="flex flex-shrink-0 items-center">
                   <img
-                    className="h-8 w-auto"
+                    className="h-14 w-auto"
                     src={myLogo}
                     alt="logo"
                   />
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
+
+                  {/* control the whole bloc of nav bar */}
+
+                  <div className="flex space-x-4 pt-3">
                     {navigation.map((item) => (
                       <a
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-black hover:text-white',
+                          item.current ? ' text-grey-700  hover:text-[#0088cc]' : 'text-gray-800  hover:text-[#0088cc]',
                           'rounded-md px-3 py-2 text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
@@ -68,7 +72,8 @@ function classNames(...classes) {
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
 
                 {/* download CV */}
-              <a href={myResume}><button className='border-2 bg-black rounded-xl '>Download CV</button></a>
+              <a href={myResume}><button className='border-2 bg-[#eeeeee] border-gray-800 
+              py-2 px-5 rounded-3xl text-sm'>Download CV</button></a>
               </div>
             </div>
           </div>
--- src/Portfolio.js
+++ src/Portfolio.js
@@ -6,7 +6,7 @@ import {dataPortfolio} from './Data'
 const Portfolio = () => {
   return (
     <div>
-      <h2 className=' targetTitle text-center py-14 text-3xl md:text-5xl font-bold'>Portfolio</h2>
+      <h2 className=' targetTitle text-center py-10 text-3xl md:text-5xl font-bold'>Portfolio</h2>
       <div>
       <Card data={dataPortfolio}/>
       </div>
