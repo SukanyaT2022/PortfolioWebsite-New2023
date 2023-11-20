9d634bd83526c79f6bb594fce86a91e27162fd3f
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-11-20 08:57:33 -0500

    add

--- src/Component/Navigation.js
+++ src/Component/Navigation.js
@@ -8,7 +8,7 @@ import myResume from '../Images/TSresume.docx'
 const navigation = [
   { name: 'Home', href: '/', current: true },
   { name: 'Resume', href: 'resume', current: false },
-  { name: 'About Me', href: 'aboutme', current: false },
+  { name: 'About Me', href: 'aboutme', current: false},
   { name: 'Portfolio', href: 'portfolio', current: false },
   { name: 'React Projects', href: 'reactportfolio', current: false },
   { name: 'IOS & Swift Projects', href: 'swiftportfolio', current: false },
@@ -27,7 +27,7 @@ function classNames(...classes) {
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 md:py-2 ">
             <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <div className="absolute inset-y-0 left-0 flex items-center lg:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="absolute -inset-0.5" />
@@ -39,16 +39,16 @@ function classNames(...classes) {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex flex-1 items-center justify-center lg:items-stretch lg:justify-start">
                 {/* targetLogo */}
                 <div className="flex flex-shrink-0 items-center">
-                  <img
+                  <a href='/'><img
                     className="h-14 w-auto"
                     src={myLogo}
                     alt="logo"
-                  />
+                  /></a>
                 </div>
-                <div className="hidden sm:ml-6 sm:block">
+                <div className="hidden lg:ml-6 lg:block">
 
                   {/* control the whole bloc of nav bar */}
 
@@ -69,7 +69,7 @@ function classNames(...classes) {
                   </div>
                 </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 lg:static lg:inset-auto lg:ml-6 lg:pr-0">
 
                 {/* download CV */}
               <a href={myResume}><button className='border-2 bg-[#eeeeee] border-gray-800 
@@ -78,7 +78,7 @@ function classNames(...classes) {
             </div>
           </div>
 
-          <Disclosure.Panel className="sm:hidden">
+          <Disclosure.Panel className="lg:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
               {navigation.map((item) => (
                 <Disclosure.Button
@@ -86,7 +86,7 @@ function classNames(...classes) {
                   as="a"
                   href={item.href}
                   className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    item.current ? ' text-gray-700 hover:text-[#0088cc]' : 'text-gray-700 hover:text-[#0088cc]',
                     'block rounded-md px-3 py-2 text-base font-medium'
                   )}
                   aria-current={item.current ? 'page' : undefined}
--- src/Home.js
+++ src/Home.js
@@ -4,8 +4,15 @@ const Home = () => {
   return (
     <div>
       <h2>this is home page</h2>
+      <a href ="contact"><button>Contact Me</button></a>
     </div>
   )
 }
 
 export default Home
+
+// background image
+// https://img.freepik.com/free-vector/blue-background-with-abstract-waves_1393-258.jpg?size=626&ext=jpg&ga=GA1.1.1608506599.1700221497&semt=sph
+
+
+// https://images.unsplash.com/photo-1513151233558-d860c5398176?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDJ8fGJhY2tncm91bmQlMjBibHVlfGVufDB8MHwwfHx8MA%3D%3D
